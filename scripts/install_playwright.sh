3a3f4bcdc7a19ce9c5d70465949aebcad227d6e0
Muhammad Abdullah <not.muhammad.abdullah68@gmail.com>
2024-02-26 11:32:12 +0500

    fixed: add path

--- scripts/install_playwright.sh
+++ scripts/install_playwright.sh
@@ -1,5 +1,6 @@
 pip3 install playwright
-echo "export PATH=\$PATH:/home/ubuntu/.local/bin" >> $HOME/.bashrc
-source $HOME/.bashrc
+# echo "export PATH=\$PATH:/home/ubuntu/.local/bin" >> $HOME/.bashrc
+# source $HOME/.bashrc
+add_path("/home/ubuntu/.local/bin")
 playwright install
 playwright install-deps
