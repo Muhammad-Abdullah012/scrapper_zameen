0b7996a2501eaa0446096437120c56f56b7ba342
Muhammad Abdullah <not.muhammad.abdullah68@gmail.com>
2024-05-22 19:37:19 +0500

    Implemented lock for cronjob

--- cronjob.py
+++ cronjob.py
@@ -15,12 +15,35 @@ SCRIPT_PATH = os.getcwd() + "/scrapper_main.py"
 
 print("SCRIPT_PATH ==>> ", SCRIPT_PATH)
 
+LOCK_FILE = os.getcwd() + "/scrapper_job.lock"
+
+
+def create_lock():
+    with open(LOCK_FILE, "w") as lockfile:
+        lockfile.write(str(os.getpid()))
+
+
+def remove_lock():
+    if os.path.exists(LOCK_FILE):
+        os.remove(LOCK_FILE)
+
+
+def is_locked():
+    return os.path.exists(LOCK_FILE)
+
 
 def job():
+    if is_locked():
+        print("Job is already running. Exiting this instance.")
+        return
+
     try:
+        create_lock()
         subprocess.run(["python3", SCRIPT_PATH], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running the script: {e}")
+    finally:
+        remove_lock()
 
 
 def threaded_job():
